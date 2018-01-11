560a02953a64180d114a0d52c9293c5f10a6c9b0
Rousan Ali <ariyankhan46@gmail.com>
2018-01-12 01:25:31 +0530

    deleted fusioncharts folder

--- fusioncharts/themes/fusioncharts.theme.fint.d.ts
+++
@@ -1,8 +0,0 @@
-
-import { FusionChartStatic } from "fusioncharts";
-
-declare namespace Fint {}
-declare var Fint: (H: FusionChartStatic) => FusionChartStatic;
-export = Fint;
-export as namespace Fint;
-
