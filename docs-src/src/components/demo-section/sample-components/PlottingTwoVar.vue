dc22836a9914935775ceac0d00a2f0b2f7a28fff
Rohan Dey <rohanoid5@gmail.com>
2019-03-20 14:31:03 +0530

    Added build files for new samples

--- docs-src/src/components/demo-section/sample-components/PlottingTwoVar.vue
+++ docs-src/src/components/demo-section/sample-components/PlottingTwoVar.vue
@@ -52,34 +52,6 @@ var schemaFetch = fetch(
 
 import FusionCharts from "fusioncharts";
 
-const jsonData = `{
-    "chart": {
-        "caption": "Recommended Portfolio Split",
-        "subCaption" : "For a net-worth of $1M",
-        "showValues":"1",
-        "showPercentInTooltip" : "0",
-        "numberPrefix" : "$",
-        "enableMultiSlicing":"1",
-        "theme": "fusion"
-    },
-    "data": [{
-        "label": "Equity",
-        "value": "300000"
-    }, {
-        "label": "Debt",
-        "value": "230000"
-    }, {
-        "label": "Bullion",
-        "value": "180000"
-    }, {
-        "label": "Real-estate",
-        "value": "270000"
-    }, {
-        "label": "Insurance",
-        "value": "20000"
-    }]
-}`;
-
 export default {
   data() {
     return {
