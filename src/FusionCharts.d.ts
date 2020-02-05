3cff5a83b1ed528e2b19341de163d5858b137600
Rohan Dey <rohanoid5@gmail.com>
2020-02-05 12:32:57 +0530

    Added definition file to support TypeScript import

---
+++ src/FusionCharts.d.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface State {
+  dataJson: any;
+  schemaJson: any;
+}
+
+declare class FusionCharts extends React.Component<any, State> {}
+
+export default FusionCharts;
