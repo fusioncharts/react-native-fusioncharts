cc6131b22ace6f5d877d570a19dc3fd825ffd752
Anik <anik.j@appwrk.com>
2024-02-05 14:28:59 +0530

    Wrapper made compatible with latest React native

--- bin/fc-builder.js
+++ bin/fc-builder.js
@@ -7,9 +7,9 @@ delete file.dependencies["react"];
 delete file.dependencies["react-native"];
 
 file.peerDependencies = {
-  "react": "^0.14.0 || ^15.0.0 || ^16.0.0 || ^17.0.0 || ^18.0.0 || ^19.0.0",
-  "react-native": "^0.64.0 || ^0.65.0 || ^0.66.0 || ^0.67.0 || ^0.68.0 || ^0.69.0 || ^0.70.0",
-  "react-native-webview": "^11.0.0"
+  "react": "^18.0.0 || ^19.0.0",
+  "react-native": "^0.73.0",
+  "react-native-webview": "^13.7.0"
 };
 
 fs.writeFile(path.join(__dirname, fileName), JSON.stringify(file, null, 2),
